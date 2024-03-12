42c3a567bf9ffd19e87c1497f38184c123528fc5
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-12 20:20:42 -0300

    Remoção de comentários desnecessários

--- src/main/java/com/ruanmattos/main/ExercicioForDivisao.java
+++ src/main/java/com/ruanmattos/main/ExercicioForDivisao.java
@@ -30,60 +30,6 @@ public class ExercicioForDivisao {
                 System.out.printf("Resultado da divisão: %.2f\n", divisao);
             }
         }
-        /*FORA:
-        switch ((int) dividendo) {
-        case 1 -> {
-        switch ((int) dividendo) {
-        case 2 -> {
-        break FORA;
-        }
-        default -> throw new AssertionError();
-        }
-        }
-        default ->
-        throw new AssertionError();
-        }*/
-        /*OUTER:
-        while (true) {
-        System.out.println("1 - Digitar um número para realizar a divisão pelo número " + dividendo);
-        System.out.println("2 - Sair do programa");
-        System.out.print("Qual opção você deseja: ");
-        int opcao = s.nextInt();
-        switch (opcao) {
-        case 1 -> {
-        System.out.print("Digite o número que deseja ser o dividendo do número " + dividendo + ": ");
-        int divisor = s.nextInt();
-        float divisao = dividendo / divisor;
-        if (divisao <= 0) {
-        System.out.println("Dívisão impossível");
-        } else {
-        System.out.println("Resultado da divisão: " + divisao);
-        }
-        }
-        case 2 -> {
-        System.out.println("Obrigado por usar o programa!");
-        break OUTER;
-        }
-        default ->
-        System.out.println("Opção inválida, tente novamente!");
-        }
-        }*/
     }
-    /*
-    if (opcao == 1) {
-                System.out.print("Digite o número que deseja ser o dividendo do número " + divisor + ": ");
-                int dividendo = s.nextInt();
-                int divisao = divisor / dividendo;
-                if (divisao == 0) {
-                    System.out.println("Dívisão impossível");
-                } else {
-                    System.out.println("Resultado da divisão: " + divisao);
-                }
-            } else if (opcao == 2) {
-                System.out.println("Obrigado por usar o programa!");
-                break;
-            } else {
-                System.out.println("Opção inválida, tente novamente!");
-            }
-     */
+
 }
